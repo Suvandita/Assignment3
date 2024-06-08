b65e1692c1433eee1086debb48b9a09a06d4a110
Suvandita <suvandita.swaroop@axeno.co>
2024-06-09 01:16:55 +0530

    fourth commit

---
+++ assignment3.js
@@ -0,0 +1,13 @@
+let button_element= document.getElementsByClassName("btn")[0];
+        let video_status=document.getElementsByTagName("video")[0];
+        
+        button_element.addEventListener("click",function (){
+            if(button_element.innerHTML=="Play"){
+                button_element.innerHTML="Pause";
+                video_status.pause();
+            }
+            else{
+                button_element.innerHTML="Play";
+                video_status.play();
+            }
+        })
