9932444186889bca655cfc1b6eeac8821b905278
Suvandita <suvandita.swaroop@axeno.co>
2024-06-10 16:46:23 +0530

    commit

--- assignment3.js
+++ assignment3.js
@@ -11,3 +11,18 @@ let video_status=document.getElementsByTagName("video")[0];
                 video_status.pause();
             }
         })
+
+
+let element= document.getElementsByClassName("click_dropbtn")[0];
+let count=0;
+element.addEventListener("click",function(){
+    count++;
+    if(count%2==0){
+        document.getElementsByClassName("click_dropdown-content")[0].style.display="none";
+        console.log(count);
+    }
+    else{
+        document.getElementsByClassName("click_dropdown-content")[0].style.display="block";
+    }
+
+})
